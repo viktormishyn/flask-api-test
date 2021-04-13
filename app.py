0e1cd45bd6b11b7a73febc8b0369b21dbdcc7bd1
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-13 13:27:24 +0300

    Add app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+
+@app.route('/super_simple')
+def super_simple():
+    return 'Hello from Planetary API!!'
+
+
+if __name__ == '__main__':
+    app.run()
