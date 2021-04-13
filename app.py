ca0c67b957e84b3526246c6c263456b5c0b7e31e
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-13 15:50:09 +0300

    GET request using url query params

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,17 @@ def hello_world():
 @app.route('/super_simple')
 def super_simple():
     return jsonify(message='Hello from Planetary API!!'), 200
+
+
+@app.route('/parameters')
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message=f"Sorry, {name}, you are not old enough."), 401
+    else:
+        return jsonify(message=f"Welcome, {name}, you are old enough!")
+
 
 
 if __name__ == '__main__':
