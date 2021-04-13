d3fe0c989a0f06da2ea08fe2dbd30bb554b63bbe
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-13 17:58:23 +0300

    Add 'db_create', 'db_drop' and 'db_seed' @app.cli.command functions in app.py

--- app.py
+++ app.py
@@ -6,6 +6,81 @@ from config import TestingConfig
 app = Flask(__name__)
 app.config.from_object(TestingConfig)
 
+# ================== DATABASE ==================
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
+
+
+# CLI: flask <command>
+
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created!')
+
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database dropped!')
+
+
+@app.cli.command('db_seed')
+def db_seed():
+    mercury = Planet(planet_name="Mercury",
+                     planet_type="Class D",
+                     home_star="Sol",
+                     mass=3.258e23,
+                     radius=1516,
+                     distance=35.98e6)
+    venus = Planet(planet_name="Venus",
+                   planet_type="Class K",
+                   home_star="Sol",
+                   mass=4.258e23,
+                   radius=3760,
+                   distance=77.24e6)
+    earth = Planet(planet_name="Earth",
+                   planet_type="Class M",
+                   home_star="Sol",
+                   mass=5.972e24,
+                   radius=3959,
+                   distance=92.96e6)
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(first_name="William",
+                     last_name="Herschel",
+                     email="william_hershel@gmail.com",
+                     password="password")
+    db.session.add(test_user)
+
+    db.session.commit()
+    print('Database seeded!')
+
+
+# ================== ROUTES ==================
+
 
 @app.route('/')
 def hello_world():
@@ -33,6 +108,7 @@ def url_variables(name: str, age: int):
         return jsonify(message=f"Sorry, {name}, you are not old enough."), 401
     else:
         return jsonify(message=f"Welcome, {name}, you are old enough!")
+
 
 
 if __name__ == '__main__':
