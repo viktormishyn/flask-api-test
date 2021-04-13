05de1f55f59685c5c181a5dcb6f0a156291873bc
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-13 16:07:14 +0300

    GET request using url variables

--- app.py
+++ app.py
@@ -23,6 +23,14 @@ def parameters():
         return jsonify(message=f"Welcome, {name}, you are old enough!")
 
 
+@app.route('/url_variables/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message=f"Sorry, {name}, you are not old enough."), 401
+    else:
+        return jsonify(message=f"Welcome, {name}, you are old enough!")
+
+
 
 if __name__ == '__main__':
     app.run()
