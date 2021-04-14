6555968e32a5654b966b9e16338864c9e1827650
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-14 11:49:22 +0300

    Create '/api/register/' endpoint

--- app.py
+++ app.py
@@ -138,5 +138,21 @@ def planets():
     return jsonify(result)
 
 
+@app.route('/api/register', methods=['POST'])
+def register():
+    email = request.form['email']
+    test = User.query.filter_by(email=email).first()
+    if test:
+        return jsonify(message='That email already exists.'), 409
+    else:
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
+        user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(message='User created successfully'), 201
+
+
 if __name__ == '__main__':
     app.run()
