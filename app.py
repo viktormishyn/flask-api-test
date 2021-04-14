ca07c1dc02dde55ed169b845089b18b0cfb18652
Viktor Mishyn <viktormishyn@gmail.com>
2021-04-14 21:34:13 +0300

    Create '/api/add_planet' endpoint

--- app.py
+++ app.py
@@ -198,5 +198,29 @@ def planet_details(planet_id: int):
         return jsonify(message="That planet does not exist"), 404
 
 
+@app.route('/api/add_planet', methods=['POST'])
+def add_planet():
+    planet_name = request.form['planet_name']
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify(message="There is already planet by that name."), 409
+    else:
+        planet_type = request.form['planet_type']
+        home_star = request.form['home_star']
+        mass = float(request.form['mass'])
+        radius = float(request.form['radius'])
+        distance = float(request.form['distance'])
+
+        new_planet = Planet(planet_name=planet_name,
+                            planet_type=planet_type,
+                            home_star=home_star,
+                            mass=mass,
+                            radius=radius,
+                            distance=distance)
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(message="You added a planet"), 201
+
+
 if __name__ == '__main__':
     app.run()
